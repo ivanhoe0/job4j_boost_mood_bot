6956f465620dd89e89f9aad66d394755e8a7eeda
corsair <corsair954@mail.ru>
2025-12-23 18:49:24 +0300

    Настройка окружения *.properties [#505377]

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -1,10 +1,21 @@
 package ru.job4j.bmb;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет. Мир");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner checkEnv(ApplicationContext ctx) {
+        return args -> {
+            System.out.println(ctx.getEnvironment().getProperty("telegram.bot.name"));
+        };
     }
 }
