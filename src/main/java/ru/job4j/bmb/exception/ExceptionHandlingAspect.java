04e95778ce690de69e4f5c531bf687c642a2101c
corsair <corsair954@mail.ru>
2026-01-01 18:59:58 +0300

    Обработка исключений с использованием AOP [#505310]

---
+++ src/main/java/ru/job4j/bmb/exception/ExceptionHandlingAspect.java
@@ -0,0 +1,18 @@
+package ru.job4j.bmb.exception;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExceptionHandlingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlingAspect.class);
+
+    @AfterThrowing(pointcut = "execution(* ru.job4j.bmb.service.*.*(..))", throwing = "ex")
+    public void handleException(Exception ex) {
+        LOGGER.error("An error occurred: {}", ex.getMessage());
+    }
+}
